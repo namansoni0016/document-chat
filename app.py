cc40a0a33bf6cad89b42c55688ffd86f12a07cc7
Naman Soni <namantsoni@gmail.com>
2025-10-08 20:23:54 +0530

    Generating response

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ class HFEmbeddingFunction:
         self.model_name = model_name
         self.model = SentenceTransformer(model_name)
 
+    def embed_documents(self, input: list[str]) -> list[list[float]]:
+        return self.model.encode(input).tolist()
+    
+    def embed_query(self, input: list[str]) -> list[list[float]]:
+        return self.model.encode(input).tolist()
+
     def __call__(self, input: list[str]) -> list[list[float]]:
         return self.model.encode(input).tolist()
     
@@ -56,11 +62,12 @@ def split_text(text, chunk_size=1000, chunk_overlap=20):
         start = end - chunk_overlap
     return chunks
 
-# Loading documents
+# Loading documents from directory
 directory_path = "./news_articles"
 documents = load_documents_from_directory(directory_path)
 print(f"Loaded {len(documents)} documents.")
 
+# Split documents into chunks
 chunked_documents = []
 for doc in documents:
     chunks = split_text(doc["text"])
@@ -82,3 +89,45 @@ for doc in chunked_documents:
     doc["embedding"] = get_hf_embeddings(doc["text"])
 
 print(doc["embedding"])
+
+# Inserting in db
+for doc in chunked_documents:
+    print("--- Inserting into chrom₹aDB ---")
+    collection.upsert(ids=[doc["id"]], documents=[doc["text"]], embeddings=[doc["embedding"]])
+
+# Query documents
+def query_documents(question, n_results=2):
+    results = collection.query(query_texts=question, n_results=n_results)
+    # Extract the relavant chunks
+    relevant_chunks = [doc for sublist in results['documents'] for doc in sublist]
+    print("--- Returning relevant chunks ---")
+    return relevant_chunks
+
+# Generate answer using Groq
+def generate_response(question, relevant_chunks):
+    context = "\n\n".join(relevant_chunks)
+    prompt = (
+        "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."
+        "\n\nContext:\n" + context + "\n\nQuestion:\n" + question
+    )
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "system",
+                "content": prompt
+            },
+            {
+                "role": "user",
+                "content": question,
+            },
+        ],
+    )
+    answer = response.choices[0].message
+    return answer
+
+# Example usage
+question = "Tell me about databricks"
+relevant_chunks = query_documents(question)
+answer = generate_response(question, relevant_chunks)
+print(answer)
